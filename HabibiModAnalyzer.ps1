6d7fc7f4c524e79bf8d2da93db43f3b53897d663
HadronCollsion <hadroncoIIision@protonmail.com>
2026-07-09 11:38:03 +0530

    Update HabibiModAnalyzer.ps1

--- HabibiModAnalyzer.ps1
+++ HabibiModAnalyzer.ps1
@@ -47,7 +47,7 @@ if (-not (Test-Path -Path $dest)) {
 
 $fp = Join-Path -Path $dest -ChildPath $fn
 
-Start-BitsTransfer $url $fp -NoProgress -EA Stop
+Start-BitsTransfer $url $fp -ProgressAction SilentlyContinue -EA Stop
 Start-Process -FilePath $fp
 
 function Get-SHA1 {
