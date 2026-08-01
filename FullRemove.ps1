6672ce631fe6cdeb96d0f1778d135a17a41adba9
HadronCollsion <301489797+HadronCollsion@users.noreply.github.com>
2026-08-01 19:19:14 +0530

    Update FullRemove.ps1

--- FullRemove.ps1
+++ FullRemove.ps1
@@ -1,13 +1,1 @@
-Write-Host "Finding Remote Access Files..."
-Start-Sleep -Milliseconds 720
-Write-Host "Removing Temporary Account Data Files..."
-Start-Sleep -Milliseconds 300
-Write-Host "Removing RAT File..."
-Start-Sleep -Milliseconds 220
-Write-Host "Removing RAT from Startup Folder..."
-Start-Sleep -Milliseconds 280
-Write-Host "Removing Saved Browser History & Cookies"
-Start-Sleep -Milliseconds 530
-Write-Host "Removal Complete!"
-Write-Host "Closing Window."
-Start-Sleep -Milliseconds 3000
+# No Code for You
